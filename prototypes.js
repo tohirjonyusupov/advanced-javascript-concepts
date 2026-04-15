68037917bf8c35357b50acb40e1ba16d1f015265
Tohirjon Yusupov <yusupovtohir12@gmail.com>
2026-04-15 19:49:38 +0500

    Prototypes && Inheritance

---
+++ prototypes.js
@@ -0,0 +1,2 @@
+// Prototype nima?
+// JavaScript'da har bir ob'ektning yashirin [[Prototype]] xususiyati bor — u boshqa ob'ektga ishora qiladi. O'zgaruvchi topilmasa, JS uni prototype zanjiri bo'ylab qidiradi.
